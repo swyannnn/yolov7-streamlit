b18c90f69da702993d9168f6a51761d8f8609078
swyannnn <swyannnn@gmail.com>
2022-08-19 00:23:30 +0800

    map.py borned

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def main():
 
 def detect(img):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default="yolov7.pt", help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default="best(2).pt", help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='Inference/', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.6, help='object confidence threshold')
